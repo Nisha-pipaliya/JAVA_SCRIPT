e1a5067bb12301b17520c699daa6576397640557
Nisha-pipaliya <nishapipaliya12@gmail.com>
2024-04-24 20:22:56 +0530

    24/4 lec-8

--- LECTURE_8/QUESTION_4/index.js
+++ LECTURE_8/QUESTION_4/index.js
@@ -3,7 +3,7 @@ const printNum=()=> {
     var num = parseInt(document.getElementById("numberInput").value);
 
     if ( num <= 0) {
-        alert("Please enter a valid positive number.");
+        alert(" Enter a valid number.");
         return;
     }
     for (let i = 1; i <= num; i++) {

99196b098f900a0ec76bae9b5a32b3e70189eca1
Nisha-pipaliya <nishapipaliya12@gmail.com>
2024-04-30 16:56:04 +0530

    lec_11 30/04

---
+++ LECTURE_11/QUESTION_2/script2.js
@@ -0,0 +1,6 @@
+
+// Maximum in array
+let arr = [2, 3, 4, 5, 6, 9, 10, 3];
+
+arr.sort((a,b)=>a-b)
+console.log(arr[arr.length-1]);
---
+++ LECTURE_11/QUESTION_3/script2.js
@@ -0,0 +1,4 @@
+// Minimum in array
+var arr = [2, 3, 4, 5, 6, 9, 10, 3];
+arr.sort((a,b)=>b-a)
+console.log(arr[arr.length-1]);

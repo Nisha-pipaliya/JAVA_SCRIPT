2c544223744c1a8ccbf2f14079bc730066863660
Nisha-pipaliya <nishapipaliya12@gmail.com>
2024-07-22 14:02:19 +0530

    22/07/24

--- FINAL_EXAM_1/js/addCart.js
+++ FINAL_EXAM_1/js/addCart.js
@@ -1,46 +1,27 @@
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   
-//     const handleData = (e) => {
-//         e.preventDefault();
-//         let newFood = {
-//             title: document.getElementById("food-name").value,
-//             img: document.getElementById("img").value,
-//             price: parseFloat(document.getElementById("food-price").value),  
-//             description: document.getElementById("food-description").value,  
-//             id: food-form.length === 0 ? 1 : food-form[food-form.length - 1].id + 1
-//         };
-//         places.push(newFood);
-//         localStorage.setItem("food-form", JSON.stringify(food-form));
-//         window.location.href = "/index.html"
-//     };
-
-//     document.getElementById("food-form").addEventListener("submit", handleData);
-// });
-
-
-// let isLogin = localStorage.getItem("isLogin") || false;
-// let userdetails = JSON.parse(localStorage.getItem("user"));
-
-// if (!isLogin) {
-//     window.location.href = "/signup.html"; 
-// }
-
-let places = JSON.parse(localStorage.getItem("places")) || [];
-
-const handleData = (e) => {
-    e.preventDefault();
-    let product = {
-        title: document.getElementById("title").value,
-        img: document.getElementById("img").value,
-        cost: parseFloat(document.getElementById("package").value),  
-        description: document.getElementById("description").value,
-        likes: 0,  
-        shares: 0,  
-        comments: [],  
-        id: places.length === 0 ? 1 : places[places.length - 1].id + 1
+    const handleData = (e) => {
+        e.preventDefault();
+        let newFood = {
+            title: document.getElementById("food-name").value,
+            img: document.getElementById("img").value,
+            price: parseFloat(document.getElementById("food-price").value),  
+            description: document.getElementById("food-description").value,  
+            id: food-form.length === 0 ? 1 : food-form[food-form.length - 1].id + 1
+        };
+        places.push(newFood);
+        localStorage.setItem("food-form", JSON.stringify(food-form));
+        window.location.href = "/index.html"
     };
-    places.push(product);
-    localStorage.setItem("places", JSON.stringify(places));
-    window.location.href = "/index.html";
-};
-document.getElementById("PlaceData").addEventListener("submit", handleData);
+
+    document.getElementById("food-form").addEventListener("submit", handleData);
+});
+
+
+let isLogin = localStorage.getItem("isLogin") || false;
+let userdetails = JSON.parse(localStorage.getItem("user"));
+
+if (!isLogin) {
+    window.location.href = "/signup.html"; 
+}
+
